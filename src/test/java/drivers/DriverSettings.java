11a9e4de9685ed87be809bb629feb974034ab625
nelak <elakovnikita24@yandex.ru>
2022-05-13 15:16:34 +0300

    fix driver settings

--- src/test/java/drivers/DriverSettings.java
+++ src/test/java/drivers/DriverSettings.java
@@ -3,13 +3,13 @@ package drivers;
 public class DriverSettings {
     public static String getDeviceProvider(String deviceProvider) {
         if (deviceProvider.equals("mobile")) {
-            return EmulatorMobileDriver.class.getName();
+            return DeviceMobileDriver.class.getName();
         }
         if (deviceProvider.equals("browserstack")) {
             return BrowserstackMobileDriver.class.getName();
         }
         if (deviceProvider.equals("emulator")) {
-            return DeviceMobileDriver.class.getName();
+            return EmulatorMobileDriver.class.getName();
         }
         throw new RuntimeException("Didn't select device");
     }
